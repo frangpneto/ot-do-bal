37b371fbddd540b44612f5f817ac7a1e8389a759
Francisco Goncalves Pereira Neto <francisco.neto@multiar.com.br>
2022-03-06 20:55:01 -0300

    ajuste boss gnomevil e abyssador

--- data/scripts/talkactions/player/addondoll.lua
+++ data/scripts/talkactions/player/addondoll.lua
@@ -40,7 +40,7 @@ function addon.onSay(player, words, param)
 
 	if player:getItemCount(addondoll_id) > 0 then
 		local word = outfits[string.lower(param)]
-		player:sendCancelMessage("Adicionando addon " .. word)
+		player:sendTextMessage(MESSAGE_INFO_DESCR, "Adicionando addon " .. word)
 		if param ~= "" and word then
 			if (not player:hasOutfit(word[1], 3) or not player:hasOutfit(word[2], 3)) and player:removeItem(addondoll_id, 1) then
 				player:getPosition():sendMagicEffect(CONST_ME_GIFT_WRAPS)
