f5c456b5d1f1ab5f8e4b961775201680a63615f6
Francisco Goncalves Pereira Neto <francisco.neto@multiar.com.br>
2022-03-19 00:06:53 -0300

    ajuste das reward

--- data/scripts/custom/rewardToAdvanced.lua
+++ data/scripts/custom/rewardToAdvanced.lua
@@ -8,21 +8,21 @@ function onAdvance(player, skill, oldlevel, newlevel)
         return true
     end
 
-    if newlevel >= 20  and player:getStorageValue(storage) ~= 10 then
+    if newlevel == 20  and player:getStorageValue(storage) ~= 10 then
         player:setStorageValue(storage, 10)
         player:setBankBalance(player:getBankBalance() + 20000)
         player:addItem(crystal, 2)
         player:sendTextMessage(MESSAGE_STATUS_CONSOLE_ORANGE, "You receive 2 crystal coin in your bank.")
-    elseif newlevel >= 60  and player:getStorageValue(storage2) ~= 10 then
+    elseif newlevel == 50  and player:getStorageValue(storage2) ~= 10 then
         player:setStorageValue(storage2, 10)
-        player:setBankBalance(player:getBankBalance() + 100000)
+        player:setBankBalance(player:getBankBalance() + 40000)
         player:addItem(crystal, 10)
-        player:sendTextMessage(MESSAGE_STATUS_CONSOLE_ORANGE, "You receive 10 crystal coin in your bank.")
-    elseif newlevel >= 100  and player:getStorageValue(storage3) ~= 10 then
+        player:sendTextMessage(MESSAGE_STATUS_CONSOLE_ORANGE, "You receive 4 crystal coin in your bank.")
+    elseif newlevel == 100  and player:getStorageValue(storage3) ~= 10 then
         player:setStorageValue(storage3, 10)
-        player:setBankBalance(player:getBankBalance() + 140000)
+        player:setBankBalance(player:getBankBalance() + 60000)
         player:addItem(crystal, 14)
-        player:sendTextMessage(MESSAGE_STATUS_CONSOLE_ORANGE, "You receive 14 crystal coin in your bank.")
+        player:sendTextMessage(MESSAGE_STATUS_CONSOLE_ORANGE, "You receive 6 crystal coin in your bank.")
     end
     return true
 end
